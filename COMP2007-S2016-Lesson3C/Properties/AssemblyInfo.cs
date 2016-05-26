094d06ec500a2b5117999d597bd0f755d6438c0e
ChristianS25 <ChristianRSimpson25@gmail.com>
2016-05-26 19:18:49 -0400

    Changed the names

--- COMP2007-S2016-Lesson3C/Properties/AssemblyInfo.cs
+++ COMP2007-S2016-Lesson3C/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("COMP2007_S2016_Lesson3C")]
+[assembly: AssemblyTitle("COMP2007_Lab3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
